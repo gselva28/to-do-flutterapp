f0e24508c0237da4fb15f32f05ff7638ab053ba2
GSelvakumar <gselvakumarofficial@gmail.com>
2020-08-04 16:20:08 +0530

    to do app

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/models/task_data.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String newTaskTitle;
+    return Container(
+      color: Color(0xFF757575),
+      child: Container(
+        padding: EdgeInsets.all(35.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newTaskTitle = newText;
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            FlatButton(
+              padding: EdgeInsets.only(
+                top: 15.0,
+                bottom: 15.0,
+              ),
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                Provider.of<TaskData>(context).addTask(newTaskTitle);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
